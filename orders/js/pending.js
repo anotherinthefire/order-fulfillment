c982a8edec9db61eade600abdd570da4bba45e68
anotherinthefire <rongodfreyultra@gmail.com>
2023-05-23 18:51:46 +0800

    final

---
+++ orders/js/pending.js
@@ -0,0 +1,10 @@
+var actionButtons = document.getElementsByClassName("action-button");
+
+for (var i = 0; i < actionButtons.length; i++) {
+    actionButtons[i].addEventListener("click", toggleMoreOptions);
+}
+
+function toggleMoreOptions() {
+    var moreOptions = this.nextElementSibling;
+    moreOptions.classList.toggle("show");
+}
